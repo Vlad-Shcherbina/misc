4340f09cf52e78a3f54be55d9c889cd18a696f55
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2013-10-11 11:15:52 +0200

    Create bash_stuff.sh

---
+++ bash_stuff.sh
@@ -0,0 +1,8 @@
+# blue abbreviated command prompt
+PS1="\[\033[1;34m\].../\W$\[\033[0m\] "
+
+
+# grep colors
+export GREP_COLOR='1;32'  # bright green
+export GREP_COLOR="00;38;5;157"  # mild greenish
+unset GREP_COLOR  # restore default
