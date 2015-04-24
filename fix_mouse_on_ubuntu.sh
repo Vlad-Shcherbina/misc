ea3fde4e7cbeee8cbdf1a1a9c0cb009fe461de11
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2015-04-24 21:05:46 +0200

    Create fix_mouse_on_ubuntu.sh

---
+++ fix_mouse_on_ubuntu.sh
@@ -0,0 +1,4 @@
+# use 'xinput list' to get device number, in this case 14.
+xinput set-prop 14 "Device Accel Profile" -1
+xinput set-prop 14 "Device Accel Constant Deceleration" 1.0
+xinput set-prop 14 "Device Accel Velocity Scaling" 2.0
