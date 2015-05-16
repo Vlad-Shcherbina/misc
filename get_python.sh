76ab3afefd15662095d8befb4949652b9739b0cb
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2015-05-16 14:43:16 +0200

    Update get_python.sh

--- get_python.sh
+++ get_python.sh
@@ -11,7 +11,11 @@ cd Python-$VERSION
 ./configure --prefix=$CURRENT_PATH/localpython
 make
 make install
+# if install fails with the message 
+# 'Ignoring ensurepip failure: pip 6.0.8 requires SSL/TLS', run
+#   sudo apt-get install libssl-dev
 
-
-# and then
-# virtualenv -p localpython/bin/python3 MY_ENV_OR_WHATEVER
+# Finally,
+#   virtualenv -p localpython/bin/python3 MY_ENV_OR_WHATEVER
+# or better 
+#   localpython/bin/pyvenv MY_ENV_OR_WHATEVER
